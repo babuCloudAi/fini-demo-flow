1d11187065ea4c677001e8bf13b26e965bbda9dd
babuCloudAi <babunaik.banoth@Cloudaillc.com>
2025-08-12 20:57:21 +0530

    updatedcode ......

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,3 +1,3 @@
-export {default as AdvisorLandingPage} from './AdvisorLandingPage';
+export {default as AdvisorLandingPage} from './advisorLandingPage';
 export {default as Alerts} from './alertsAndAcoomplishments/alerts';
 export {default as Accomplishments} from './alertsAndAcoomplishments/accomplishments';
